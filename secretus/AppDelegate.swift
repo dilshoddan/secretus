ee37a008adfa0ede913bb074e300fa375d790f56
dilshoddan <dilshoddan@gmail.com>
2019-02-25 15:05:55 +0500

    add firebase

--- secretus/AppDelegate.swift
+++ secretus/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window.rootViewController = splashView
             window.makeKeyAndVisible()
         }
+        
+        FirebaseApp.configure()
         return true
     }
 
